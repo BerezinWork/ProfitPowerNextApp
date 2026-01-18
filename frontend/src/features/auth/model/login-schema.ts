7767ddc3a48e83cf7774148e6cd76173bc94b0cb
Illia Berezin <berezin.main@gmail.com>
2026-01-18 16:10:34 +0200

    Input styles

--- frontend/src/features/auth/model/login-schema.ts
+++ frontend/src/features/auth/model/login-schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, 'Password is required')
+    email: z.string().email({ message: ">> Invalid email format" }),
+    password: z.string().min(1, { message: ">> Password is required"})
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
--- frontend/src/features/auth/model/schema.ts
+++ frontend/src/features/auth/model/schema.ts
@@ -3,27 +3,27 @@ import { z } from "zod";
 export const registerSchema = z.object({
     nickname: z
         .string()
-        .min(3, { message: "Min 3 chars" })
-        .max(20, { message: "Max 20 chars" })
-        .regex(/^[a-zA-Z]/, { message: "Must start with a letter" })
-        .regex(/^[a-zA-Z0-9_-]+$/, { message: "Only letters, numbers, '-' and '_'" }),
+        .min(3, { message: ">> Nickname required" })
+        .max(20, { message: ">> Maximum 20 chars" })
+        .regex(/^[a-zA-Z]/, { message: ">> Must start with a letter" })
+        .regex(/^[a-zA-Z0-9_-]+$/, { message: ">> Only letters, numbers, '-' and '_'" }),
 
     email: z
         .string()
-        .min(1, { message: "Email is required" })
-        .email({ message: "Invalid email format" }),
+        .min(1, { message: ">> Email required" })
+        .email({ message: ">> Invalid email format" }),
 
     password: z
         .string()
-        .min(8, { message: "Min 8 chars" })
-        .regex(/[A-Z]/, { message: "Need 1 uppercase letter" })
-        .regex(/[a-z]/, { message: "Need 1 lowercase letter" })
-        .regex(/[0-9]/, { message: "Need 1 number" }),
+        .min(8, { message: ">> Password required" })
+        .regex(/[A-Z]/, { message: ">> Need 1 uppercase letter" })
+        .regex(/[a-z]/, { message: ">> Need 1 lowercase letter" })
+        .regex(/[0-9]/, { message: ">> Need 1 number" }),
 
     acceptTerms: z
         .boolean()
         .refine((val) => val === true, {
-            message: "You must accept Terms and Conditions",
+            message: ">> You must accept Terms and Conditions",
         }),
 });
 
